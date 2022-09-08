e8d9d63d576a65ce81f2d665d0a627be9c3ea1d0
Roberto Toledo <toledoroberto@gmail.com>
2022-09-07 22:05:18 -0300

    V5

--- src/main/java/com/EggNews/Controladores/NoticiaControlador.java
+++ src/main/java/com/EggNews/Controladores/NoticiaControlador.java
@@ -6,6 +6,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,11 +25,13 @@ public class NoticiaControlador {
     }
 
     @PostMapping("/altanoticia")
-    public String altanoticia(@RequestParam String titulo_noticia, @RequestParam String resumen_noticia, @RequestParam String cuerpo) {
+    public String altanoticia(@RequestParam String titulo_noticia, @RequestParam String resumen_noticia, @RequestParam String cuerpo, ModelMap modelo) {
         try {
             notServ.crearNoticia(titulo_noticia, resumen_noticia, cuerpo);
+            modelo.put("exito", "El libro fue cargado correctamente"); //(llave, valor)
         } catch (MiException ex) {
-            Logger.getLogger(NoticiaControlador.class.getName()).log(Level.SEVERE, null, ex);
+            //Logger.getLogger(NoticiaControlador.class.getName()).log(Level.SEVERE, null, ex);
+            modelo.put("error", ex.getMessage());
         return "alta_noticia.html";
         }
         return "index.html";
--- src/main/java/com/EggNews/Controladores/PortalControlador.java
+++ src/main/java/com/EggNews/Controladores/PortalControlador.java
@@ -1,16 +1,24 @@
-
 package com.EggNews.Controladores;
 
+import com.EggNews.Entidades.Noticia;
+import com.EggNews.Servicios.NoticiaServicios;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
 public class PortalControlador {
-    
-    public String index(){
-        
+
+    @Autowired
+    private NoticiaServicios notServ;
+
+    public String index(ModelMap modelo) {
+        List<Noticia> noticia= notServ.listarNoticias();
+        modelo.addAttribute("noticia",noticia);
         return "index.html";
     }
-    
+
 }
